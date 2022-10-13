c82fa20a25e6b17d88079a796e7f17a771227c6c
BarBronshtein <barbronshtein@gmail.com>
2022-10-13 12:22:37 +0300

    added app-header component

---
+++ src/components/app-header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header v-if="user" class="app-header">
+  </header>
+</template>
+
+<script lang="ts">
+import { useUserStore } from '@/stores/user';
+import { mapState } from 'pinia';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'app-header',
+  components: {},
+
+  computed: {
+    ...mapState(useUserStore, ['user']),
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
