478d5e2067086909ca2671b5948d5e2101bb9561
BarBronshtein <barbronshtein@gmail.com>
2022-10-16 18:30:19 +0300

    added avatar component

---
+++ src/components/avatar.vue
@@ -0,0 +1,27 @@
+<template>
+  <img class="avatar" :style="{width:size,height:size}" :src="img">
+</template>
+
+<script lang="ts">
+import { useUserStore, type User } from '@/stores/user';
+import { mapState } from 'pinia';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'avatar',
+  props: {
+    size: { type: Number },
+  },
+  computed: {
+    ...mapState(useUserStore, ['user']),
+    img() {
+      if (!this.user) return;
+      const curUser = this.user as User
+      return curUser.photo || `https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png`
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
