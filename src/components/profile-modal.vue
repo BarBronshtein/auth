eb7d41968ea75416182490d7f22b02b14167ddea
BarBronshtein <barbronshtein@gmail.com>
2022-10-16 20:49:11 +0300

    added profile-modal

---
+++ src/components/profile-modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="profile-modal modal">
+    <div class="modal-wrapper">
+      <div v-for="(label,i) in data" :key="label.title" class="modal-item" :class="!i ? 'active':''">
+        <span :class="`fa-solid fa-${label.icon}`"></span>
+        <span class="title">{{label.title}}</span>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'profile-modal',
+  data() {
+    return {
+      data: [{ icon: 'user', title: 'my profile' }, { icon: 'exit', title: 'logout' }]
+    }
+  },
+  methods: {},
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
