19b073b098552549a2d9d4c81d963637026f2e8e
BarBronshtein <barbronshtein@gmail.com>
2022-11-15 10:45:26 +0200

    added module-federation vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import federation from '@originjs/vite-plugin-federation';
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		federation({
+			name: 'auth-app',
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./Login': './src/views/login-form.vue',
+			},
+		}),
+	],
 	build: {
 		outDir: '../auth-app-backend/dist/public',
 		emptyOutDir: true,
